8be4f1a3547debb2cf7404482149e7eddb70a6bd
Pepe <115153537+retkes-ziggy@users.noreply.github.com>
2025-02-27 09:40:22 +0100

    vége
    Vége

--- teliolimpia.js
+++ teliolimpia.js
@@ -23,6 +23,16 @@ app.get("/",(req, res)=>{
     res.send("Müködik a szerver.")
 })
 
+app.get("/v",(req, res) =>
+    {
+        const sql = "SELECT * FROM versenyzok";
+        db.query(sql, (err, result)=>{
+            if (err)return res.status(500).json({error: err.message});
+            res.json(result)
+        })
+        
+    }
+)
 
 app.listen(3000,() =>
     {
